2e8d9ecc2a9e1412c9dcf62aa5fda9b275d44a46
Alfred Aquino <alfredaquino@hotmail.com>
2023-01-19 15:44:01 -0500

    need to verify

--- doomsday-fuel/solution.py
+++ doomsday-fuel/solution.py
@@ -1,110 +1,68 @@
 #!/usr/bin/env python3
 import json
-
+from functools import reduce
+import operator
 from fractions import Fraction
 
 def matrix_to_str(matrix):
     str_rows = []
     for row in matrix:
-        str_rows.append(str(row))
+        row_str = ""
+        for _, col in enumerate(row):
+            row_str += f"{str(col)} "
+        str_rows.append(row_str)
     return '\n' + '\n'.join(str_rows)
 
-# def get_denominators(matrix):
-#     denominators = [0] * len(matrix)
-#     for row_num, row in enumerate(matrix):
-#         denominators[row_num] = (sum(row))
-#     return denominators
 
-# def get_probabilities(matrix):
-#     denominators = get_denominators(matrix)
-#     print(f"solution: denominators: {denominators}")
-#     probabilities = []
-#     for row_num, row in enumerate(matrix):
-#         print(row_num, row)
-#         p_row = []
-#         denominator = denominators[row_num]
-#         for col in row:
-#             p_row.append((col, denominator))
-#         probabilities.append(p_row)
-#     return probabilities
 
-    
-def get_traces(matrix):
-    traces = {}
-    for state, row in enumerate(matrix):
-        state_id = f"s{state}"
-        # traces[state_id] = row
-        denominator = sum(row)
-        if denominator > 0:
-            s_next = set()
-            for s_index, s in enumerate(row):
-                if s > 0:
-                    s_id = f"s{s_index}"
-                    s_next.add((s_id, s, denominator))
-            traces[state_id] = s_next
-    return traces
-
-def propagate_probabilities(matrix):
-    print("propagate_probabilities")
+def to_fractions(matrix):
+    m2 = []
+    denominators = []
     for row_num, row in enumerate(matrix):
-        for i, r in enumerate(row):
-            p,d = r
-            if p > 0:
-                p1, d1 = matrix[i][row_num]
-                print(f"p1: {p1}, d1: {d1}... ", end='')
-                p1 *= p / d
-
-                #  TO DO: Propagate this new probability into denominators
-                
-                matrix[i][row_num] = (p1, d1)
-                print(f"matrix[{i}][{row_num}]: {matrix[i][row_num]}, ", end='')
-        print()
-    return matrix
+        d = sum(matrix[row_num])
+        m2_row = []
+        for col_num, col in enumerate(row):
+            numerator = col
+            f = f"{numerator}/{d}" if numerator > 0 else 0
+            m2_row.append(f)
+        m2.append(m2_row)
+        denominator = sum([Fraction(m).numerator for m in m2_row if m != 0])
+        denominators.append(denominator)
+    return m2, denominators
 
 def solution(m):
-    # print(f"inputs:{matrix_to_str(m)}")
-    m2 = [[Fraction(number) for number in row] for row in m]
-    # print(f"inputs:{matrix_to_str(m2)}")
-    result_rows = []
-    denominators = []
-    for row_num, row in enumerate(m2):
-        d = sum([r.numerator for r in row])
-        # print(f"iteration:{row_num} --> m:{matrix_to_str(m2)}") 
-        for i, p in enumerate(row):
-            if p > 0:
-                c = Fraction(p,d)
-                n = m2[i][row_num].numerator * c
-                m2[i][row_num] = Fraction(n, 1)
-        d = sum([r.numerator for r in row])
-        if d > 0:
-            denominators.append(d)
-        else:
-            result_rows.append(row_num)
-        print(f"status m:{matrix_to_str(m)}")
-        print(f"status m2:{matrix_to_str(m2)}")
-    print(f"denominators:{denominators}")
-    print(f"result_rows:{result_rows}")
-
-    denominator = 1
-    for d in denominators:
-        denominator *= d
-    probabilities = [0] * len(m)
-
-    m2 = m.copy()
+    m2, denominators = to_fractions(m)    
+    denominator = reduce(operator.mul, [d for d in denominators if d > 0])
+    output_rows = []
+    for i, d in enumerate(denominators):
+        if d == 0:
+            output_rows.append(i)
+    input_counts = [sum(row) for row in m]
+    input_count = reduce(operator.mul, [i for i in input_counts if i > 0])
     
-    for row_num, row in enumerate(m2):
-        for i, r in enumerate(row):
-            if r > 0:
-                
-
-                # m2[i][row_num] *= m[row_num][i]
-    print(f"new m:{matrix_to_str(m)}")
-    # if len(m) == 5:
-    #     return [7,6,8,21]
-    # if len(m) == 6:
-    #     return [0,3,2,9,14]
-    print(get_traces(m))
+    column = 0
+    row = 0
+    outputs = [0] * len(m)
+    while column < len(m) and row < len(m):
+        cell = m2[row][column]
+        
+        if Fraction(cell) > 0:
+            for i, t in enumerate(m2[column]):
+                if Fraction(t) > 0:
+                    a,b = Fraction(t).as_integer_ratio()
+                    c,d = Fraction(cell).as_integer_ratio() 
+                    new_frac = Fraction( a*c, b*d )
+                    m2[column][i] = str(new_frac)
+                    outputs[i] = input_count * Fraction(t)
+        if column in output_rows and Fraction(cell) > 0:
+            outputs[column] = Fraction(cell) * input_count
+            
+        column += 1
+        if column == len(m):
+            column = 0
+            row += 1
     
+    probabilities = [int(o) for i, o in enumerate(outputs) if i in output_rows]
     return probabilities + [denominator]
     
     
@@ -131,10 +89,12 @@ test(solution,
 expected=[7, 6, 8, 21])
 
 
+s0->s1: 2, s0->s2: 3
+s1->s3: 3, s1->s4: 4
 
-s0 -> s2: 1/3 -> 7/21
-s0 -> s1 -> s3: 2/3 * 3/7 -> 6/21
-s0 -> s1 -> s4: 2/3 * 4/7 -> 8/21
+s0->s1->s3: 2/3 * 3/7 = 6/21 
+s0->s1->s4: 2/3 * 4/7 = 8/21
+s0->s2: 1/3
 
 
 test(solution, 
@@ -148,32 +108,10 @@ test(solution,
 ], 
 expected=[0, 3, 2, 9, 14])
 
-s0 -> s1: 50%
-s0 -> s5: 50%
-s1 -> s0 -> ((1/2)*4)/9
-s2 <- 0
-s3 <- s1 <- s0: 3/9 * (1/2 + 4/9)
-s4 <- s1 <- s0: 2/9 * (1/2 + 4/9)
-s5 <- s0: 1/2
-
-
-s0 <- s1(4/9)
-s1 <- s0(1/2)
-s2 <- 0
-s3 <- s1(3/9) * s0(1/2)
-s4 <- s1(2/9) * s0(1/2)
-s5 <- s0(1/2) + s1(4/9)
-
-
-
-
-s0 -> s5
-s0 -> s1 -> s3
-s0 -> s1 -> s4
-
-1 + 1 + 4 + 3 + 2 = 11
-
-
-
+s0->s1: 1/2
+s0->s5: 1/2 ... * 18 = 9
+s1->s0:? 1/2 * 4/9 = 4/18 = 2/9 ... * 18 = 4 
+s1->s3: 1/2 * 3/9 = 3/18 ... * 18 = 3
+s1->s4: 1/2 * 2/9 = 2/18 ... * 18 = 2
 
 """
